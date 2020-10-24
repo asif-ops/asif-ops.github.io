e6fc22239031824d8180281ff3539aed40e587ed
MD ASIF HASAN RIYAD <69663700+asif-ops@users.noreply.github.com>
2020-10-24 19:40:53 +0600

    update docker_check.sh
    update docker_check.sh

--- docker_check.sh
+++ docker_check.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 val=$(sudo docker ps | wc -l)
-val2=$(sudo docker rm -f $(sudo docker ps -a -q))
 if [ $val -gt 1 ]
 then
-	 $val2
+        val2=$(sudo docker rm -f $(sudo docker ps -a -q))
 else
-	echo "No docker container running"
+        echo "No docker container running"
 fi
